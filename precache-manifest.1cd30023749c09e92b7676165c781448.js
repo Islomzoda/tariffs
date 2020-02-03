22a1f3030470b51346bcedfa6d5a1d0db9cc2e14
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-03 03:18:24 +0000

    Deploy Islomzoda/tariffs to github.com/Islomzoda/tariffs.git:gh-pages

---
+++ precache-manifest.1cd30023749c09e92b7676165c781448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650358bd946898f91be718617a09d8e0",
+    "url": "/tariffs/index.html"
+  },
+  {
+    "revision": "579c98c78bedb7067afa",
+    "url": "/tariffs/static/css/main.094dcbf3.chunk.css"
+  },
+  {
+    "revision": "1fe6aadc92e32a4799d6",
+    "url": "/tariffs/static/js/2.4b3095f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tariffs/static/js/2.4b3095f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "579c98c78bedb7067afa",
+    "url": "/tariffs/static/js/main.683287ec.chunk.js"
+  },
+  {
+    "revision": "fa951d5a2c7e4e604ba2",
+    "url": "/tariffs/static/js/runtime-main.c8da4462.js"
+  }
+]);
